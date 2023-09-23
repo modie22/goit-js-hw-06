cdb51a4fde9213f668a26416f17b2cb1501f9b2c
Kostya Semenchuk <75216256+modie22@users.noreply.github.com>
2023-09-23 19:45:44 +0300

    task_2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientseulEL = document.querySelector('#ingredients');
+//const inEl = [];
+//for(let i=0;i<ingredients.length;i++){
+//  inEl.push(document.createElement('li'));
+//  inEl[i].textContent=ingredients[i];
+//  inEl[i].classList.add('item');
+//}
+//ingredientseulEL.append(...inEl);
+//console.log(ingredientseulEL);
+const ingredientsCol = ingredients =>{
+   return ingredients.map(ingredient =>{
+    const liEl = document.createElement('li');
+    liEl.textContent=ingredient;
+    liEl.classList.add('item');
+    return liEl;
+   })
+}
+const ingredientLiEl=ingredientsCol(ingredients);
+ingredientseulEL.append(...ingredientLiEl);
