526c03e8a01cb5d12ddf070831d012e23bdaab35
Kostya Semenchuk <75216256+modie22@users.noreply.github.com>
2023-09-25 15:41:58 +0300

    task_9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const spanEl = document.querySelector(".color");
+const buttonEl = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+
+const btnFun = () => {
+  spanEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor=getRandomHexColor();
+}
+buttonEl.addEventListener("click", btnFun);
