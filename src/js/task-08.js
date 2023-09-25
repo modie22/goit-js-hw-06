1b9817768da229ff20a9aefc266d0aebc3a3b2ab
Kostya Semenchuk <75216256+modie22@users.noreply.github.com>
2023-09-25 15:34:49 +0300

    task_8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector(".login-form");
 
+const onFormSubmit = (events) => {
+    events.preventDefault();
+  const {
+    elements: { email, password },
+  } = events.currentTarget;
+
+    if (email.value === "" && password.value === "")
+        return alert("Всі поля повинні бути заповнені!");
+    
+    const user = { email: email.value, password: password.value };
+    console.log(user);
+    
+};
+
+formEl.addEventListener("submit", onFormSubmit);
